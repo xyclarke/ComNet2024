ef8e26872bc4ce955e14fcf651fb53e4a8dcbc34
xyclarke <71722643+xyclarke@users.noreply.github.com>
2024-02-04 16:01:01 -0500

    Update gettingStarted.py
    Made minor changes

--- gettingStarted.py
+++ gettingStarted.py
@@ -10,7 +10,7 @@ def welcome_assignment_answers(question):
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         return "Yes"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
-        return "Yes"
+        return "No"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         return "No"
     elif question == "What is the SHA256 hashing value to the following message: 'NYU Computer Networking Fall 2023' - Use SHA256 hash generator and use the answer in your code":
@@ -20,7 +20,7 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         return "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        return int(4)
+        return int(7)
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
         return int(3)
     elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
