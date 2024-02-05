515f1efb56880243692f1d62f348fdf6a5a54e51
xyclarke <71722643+xyclarke@users.noreply.github.com>
2024-02-04 19:06:25 -0500

    Update gettingStarted.py
        elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
            return int(5)
        elif question == "Is it possible to decode a message without a key? - Yes/No":
            return "Yes"

--- gettingStarted.py
+++ gettingStarted.py
@@ -10,9 +10,9 @@ def welcome_assignment_answers(question):
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         return "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
-        return "No"
-    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         return "Yes"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        return "No"
     elif question == "What is the SHA256 hashing value to the following message: 'NYU Computer Networking Fall 2023' - Use SHA256 hash generator and use the answer in your code":
         return "52a9b9b03b3e6113a89ae37772d20eee00d5fef678adbb1161dfd7a78c089684"
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
@@ -20,7 +20,7 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         return "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        return int(1)
+        return int(5)
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
         return int(3)
     elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
